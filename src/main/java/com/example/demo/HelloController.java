1747ac0ee978fd10c4ea16c9f1b7643270615b56
s g <te210849@gmail.com>
2024-05-21 11:30:19 +0900

    4

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.demo;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.core.model.Model;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 /**
 * Hello world!
